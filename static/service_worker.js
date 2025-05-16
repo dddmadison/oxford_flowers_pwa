7a484c3619aa7fb3d582c63380798ce4da03aee5
dddmadison <119731457+dddmadison@users.noreply.github.com>
2025-05-17 00:16:04 +0900

    Update service_worker.js

--- static/service_worker.js
+++ static/service_worker.js
@@ -2,10 +2,11 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index_pwa.html',
-        '/static/style.css',
-        '/static/script.js'
+        '/',  // 홈 경로만 캐싱
+        // 불필요한 정적 파일 경로 제거
+        '/static/icons/icon-192x192.png',
+        '/static/icons/icon-512x512.png',
+        '/static/manifest.json'
       ]);
     })
   );
